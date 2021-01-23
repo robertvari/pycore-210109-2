01bbfcc96ee7e687ed4fe863f1f279a4e08dd954
Vári Róbert <mail.robertvari@gmail.com>
2021-01-23 17:38:28 +0100

    Magic Number game

--- MagicNumber.py
+++ MagicNumber.py
@@ -21,7 +21,15 @@ def menu():
 
 
 def game():
-    pass
+    magic_number = random.randint(0, 10)
+    max_tries = 3
+
+    user_number = get_user_number()
+
+
+def get_user_number() -> int:
+    user_number = int(input("Your number?"))
+    return user_number
 
 
 if __name__ == '__main__':

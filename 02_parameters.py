b819d2b8f2122aac89e9896a8abcedc2f33d095d
Vári Róbert <mail.robertvari@gmail.com>
2021-01-23 14:50:21 +0100

    Define functions

--- 02_parameters.py
+++ 02_parameters.py
@@ -10,4 +10,18 @@ def say_hello(name, age):
     print(f"Hello {name}. You are {age} old.")
 
 
-say_hello("Robert", 32)
+# say_hello("Robert", 32)
+# say_hello(age=32, name="Robert")
+
+# default parameters
+def add_numbers(a=0, b=0):
+    print(a + b)
+
+
+def register_user(email, password, address=None, phone=None):
+    print(f"User email: {email}")
+    print(f"User password: {password}")
+    print(f"User address: {address}")
+    print(f"User phone: {phone}")
+
+# register_user("robert@gmail.com", "testpas123", address="Budapest")

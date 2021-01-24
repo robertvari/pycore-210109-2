bca295d770389c52c751a71179086309bd5bb2b1
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 15:50:35 +0100

    lambda functions

--- 09_anonim_functions.py
+++ 09_anonim_functions.py
@@ -1,2 +1,8 @@
 my_lambda = lambda a, b: a+b
 # print(my_lambda(3, 3))
+
+name_list = ["Vári Róbert", "Kiss Elemér", "Nagy Adrienn", "Tóth Barna"]
+
+# print(sorted(name_list))
+
+print(sorted(name_list, key=lambda name: name.split(" ")[-1]))

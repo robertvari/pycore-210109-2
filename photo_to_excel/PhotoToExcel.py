2ec382579e98f2b2e244bfcde66a1fc10932894b
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 17:36:18 +0100

    Get photo data

--- photo_to_excel/PhotoToExcel.py
+++ photo_to_excel/PhotoToExcel.py
@@ -1,4 +1,4 @@
-from my_modules.my_functions import get_root_path, get_photo_files
+from my_modules.my_functions import get_root_path, get_photo_files, get_photo_data
 
 
 def main():
@@ -7,6 +7,9 @@ def main():
 
     # list jpg files in path
     photos = get_photo_files(root_folder)
+
+    # collect data from images
+    photo_data_list = [get_photo_data(i) for i in photos]
 
 
 if __name__ == '__main__':

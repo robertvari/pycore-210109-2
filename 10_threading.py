1113e3d166f0a74c58620f71393f02ae849af4af
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 16:30:17 +0100

    threading

--- 10_threading.py
+++ 10_threading.py
@@ -1,13 +1,22 @@
-import random, time
+import random, time, threading
 
 
-def worker1():
-    print("Worker 1 started")
-    time.sleep(random.randint(1, 10))
+def worker1(sleep_time):
+    print(f"Worker 1 started. {threading.currentThread().name}")
+    time.sleep(random.randint(1, sleep_time))
     print("Worker 1 finished")
 
 
-def worker2():
-    print("Worker 2 started")
-    time.sleep(random.randint(1, 10))
+def worker2(sleep_time):
+    print(f"Worker 2 started. {threading.currentThread().name}")
+    time.sleep(random.randint(1, sleep_time))
     print("Worker 2 finished")
+
+
+t1 = threading.Thread(target=worker1, args=[3])
+t2 = threading.Thread(target=worker2, args=[10])
+
+t1.start()
+t2.start()
+
+print(f"All process finished on {threading.currentThread().name}")

945c32d399d876f00f1e108dc9421f38783e6287
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 15:23:27 +0100

    get all files

--- 08_recursion.py
+++ 08_recursion.py
@@ -13,8 +13,17 @@ def get_factorial(n):
 
 def find_all_files(root_folder, file_list):
     folder_content = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
-    pass
+
+    file_list += [i for i in folder_content if os.path.isfile(i)]
+
+    subfolders = [i for i in folder_content if os.path.isdir(i)]
+
+    for f in subfolders:
+        find_all_files(f, file_list)
+
 
 file_list = []
-find_all_files("D:/Photos", file_list)
-print(file_list)
+find_all_files("D:\\Photos", file_list)
+for i in file_list:
+    print(i)
+# print(len(file_list))

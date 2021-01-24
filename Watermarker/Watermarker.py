7ca0a3c0a8e75e0528edc3908b71ae843519e5f2
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 18:41:59 +0100

    Watermarker finished

--- Watermarker/Watermarker.py
+++ Watermarker/Watermarker.py
@@ -6,14 +6,19 @@ job_queue = queue.Queue()
 
 def main():
     folder_path = "D:\\Photos\\CIW"
+
+    save_folder = os.path.join(folder_path, "_watermarked")
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+
     watermark_text = "Hello World"
 
     photo_files = get_photo_files(folder_path)
 
     for i in photo_files:
-        job_queue.put({"path": i, "text": watermark_text})
+        job_queue.put({"path": i, "text": watermark_text, "save_folder": save_folder})
 
-    for _ in range(6):
+    for _ in range(8):
         t = threading.Thread(target=watermark_worker)
         t.start()
 
@@ -31,15 +36,11 @@ def watermark_worker():
     while not job_queue.empty():
         job_data = job_queue.get()
 
-        root_dir = os.path.dirname(job_data["path"])
-        save_folder = os.path.join(root_dir, "_watermarked")
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
-
         photo_path = job_data["path"]
         print(f"Working on {photo_path}...")
 
         watermark_text = job_data["text"]
+        save_folder = job_data["save_folder"]
 
         image_name = os.path.basename(photo_path)
         img = Image.open(photo_path)

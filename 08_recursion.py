fd2d46230fe550628796cf825d39706a5baf97f0
Vári Róbert <mail.robertvari@gmail.com>
2021-01-24 15:11:06 +0100

    get all files

--- 08_recursion.py
+++ 08_recursion.py
@@ -1,9 +1,20 @@
+import os
+
+
 def get_factorial(n):
     if n < 2:
         return n
     else:
         return n * get_factorial(n-1)
 
-
 # result = get_factorial(5)
 # print(result)
+
+
+def find_all_files(root_folder, file_list):
+    folder_content = [os.path.join(root_folder, i) for i in os.listdir(root_folder)]
+    pass
+
+file_list = []
+find_all_files("D:/Photos", file_list)
+print(file_list)
